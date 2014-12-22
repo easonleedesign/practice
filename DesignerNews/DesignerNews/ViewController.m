175c15f43427e0880e2f19e4de70be592f9a7884
easonlee <easonlee@easonlees-MacBook-Pro.local>
2014-12-21 16:29:50 -0800

    Animate in Xcode

--- DesignerNews/DesignerNews/ViewController.m
+++ DesignerNews/DesignerNews/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)loginButtonDidPress:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UIView *dialogView;
 
 @end
 
@@ -24,4 +27,26 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)loginButtonDidPress:(id)sender {
+    // Animate login button
+    [UIView animateWithDuration:0.1 animations:^{
+        self.loginButton.transform = CGAffineTransformMakeTranslation(10, 0);
+    } completion:^(BOOL finished){
+        // step 2
+        [UIView animateWithDuration:0.1 animations:^{
+            self.loginButton.transform = CGAffineTransformMakeTranslation(-10, 0);
+        } completion:^(BOOL finished){
+            // step 3
+            [UIView animateWithDuration:0.1 animations:^{
+                self.loginButton.transform = CGAffineTransformMakeTranslation(0, 0);
+            } completion:^(BOOL finished){
+            }];
+        }];
+    }];
+    
+    // animateWithDuration with Damping
+    [UIView animateWithDuration:0.7 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0 options:0 animations:^{
+        [self.dialogView setFrame:CGRectMake(self.dialogView.frame.origin.x, self.dialogView.frame.origin.y, self.dialogView.frame.size.width, 320)];
+    }completion:^(BOOL finished) {}];
+}
 @end
